186335638e0ec304a4b26cc7f42c35c7f484709b
kathi <k.aus.w@gmx.de>
2020-03-12 00:24:17 +0100

    basic listview for food_journal

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
--- app/src/main/java/com/example/myapplication/food_journal.java
+++ app/src/main/java/com/example/myapplication/food_journal.java
@@ -2,14 +2,55 @@ package com.example.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class food_journal extends AppCompatActivity {
 
+    ListView listView;
+    TextView textView;
+    String[] testdata ={
+            "hallo",
+            "tut",
+            "das",
+            "funktionieren?",
+            "meh",
+            "da",
+            "muss",
+            "meeeee",
+            "hhhr",
+            "ich",
+            "will",
+            "scrollen",
+            "..."
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.food_journal);
+
+        listView = (ListView)findViewById(R.id.listview);
+        textView = (TextView)findViewById(R.id.ListTextView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,testdata);
+
+        listView.setAdapter(adapter);
+
+/*        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l){
+            Object listItem = listView.getItemAtPosition(i);
+        }});*/
+
+
+
+
+
+
     }
 }
