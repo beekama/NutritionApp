c4b87ebee71895f78ae3e3a119d886974a0d2346
Yannik Schmidt <sheppy@atlantishq.de>
2020-03-17 18:29:32 +0100

    define food object structure

---
+++ app/src/main/java/com/example/myapplication/Foods.java
@@ -0,0 +1,36 @@
+package com.example.myapplication;
+
+
+class Food {
+    public String name;
+    public int energy;
+    public int fiber;
+    public Minerals minerals;
+    public Vitamins vitamins;
+
+    public Food(String name, int energy, int fiber, Minerals minerals, Vitamins vitamins){
+        this.name = name;
+        this.energy = energy;
+        this.fiber = fiber;
+        this.minerals = minerals;
+        this.vitamins = vitamins;
+    }
+}
+
+class Vitamins{
+    public int A;
+    public int C;
+    public int D;
+    public int E;
+    public int K;
+    public int B6;
+    public int B12;
+}
+
+class Minerals {
+    public int iron;
+    public int magnesium;
+    public int zinc;
+    public int calcium;
+    public int potassium;
+}
