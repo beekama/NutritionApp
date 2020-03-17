cd128203632ec3b1511109315024448177cb5f28
kathi <k.aus.w@gmx.de>
2020-03-17 01:32:28 +0100

    list view with headers

--- app/src/main/java/com/example/myapplication/food_journal.java
+++ app/src/main/java/com/example/myapplication/food_journal.java
@@ -1,22 +1,32 @@
 package com.example.myapplication;
 
-import android.app.Activity;
+
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.AdapterView;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class food_journal extends AppCompatActivity {
 
+    private ListView zeddel;
+
+
+    /*
     ListView listView;
     TextView textView;
-    String[] testdata ={
+    String[] testdata = {
             "hallo",
             "tut",
             "das",
@@ -31,27 +41,67 @@ public class food_journal extends AppCompatActivity {
             "scrollen",
             "..."
     };
-
+*/
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.food_journal);
 
-        listView = (ListView)findViewById(R.id.listview);
-        textView = (TextView)findViewById(R.id.ListTextView);
+        zeddel = (ListView) findViewById(R.id.listview);
+        ArrayList<Item> InputListe = new ArrayList<Item>();
+        //header:
+        InputListe.add(new HeaderItem("day one"));
+        //items:
+        InputListe.add(new ItemItem("bla"));
+        InputListe.add(new ItemItem("blaa"));
+        InputListe.add(new ItemItem("blaaa"));
+        InputListe.add(new ItemItem("blaaaa"));
+        //header:
+        InputListe.add(new HeaderItem("day two"));
+        //items:
+        InputListe.add(new ItemItem("bla"));
+        InputListe.add(new ItemItem("blaa"));
+        InputListe.add(new ItemItem("blaaa"));
+        InputListe.add(new ItemItem("blaaaa"));
+        //header:
+        InputListe.add(new HeaderItem("day three"));
+        //items:
+        InputListe.add(new ItemItem("bla"));
+        InputListe.add(new ItemItem("blaa"));
+        InputListe.add(new ItemItem("blaaa"));
+        InputListe.add(new ItemItem("blaaaa"));
+        //header:
+        InputListe.add(new HeaderItem("day four"));
+        //items:
+        InputListe.add(new ItemItem("bla"));
+        InputListe.add(new ItemItem("blaa"));
+        InputListe.add(new ItemItem("blaaa"));
+        InputListe.add(new ItemItem("blaaaa"));
+        //header:
+        InputListe.add(new HeaderItem("day five"));
+        //items:
+        InputListe.add(new ItemItem("bla"));
+        InputListe.add(new ItemItem("blaa"));
+        InputListe.add(new ItemItem("blaaa"));
+        InputListe.add(new ItemItem("blaaaa"));
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,testdata);
+        //set adapter:
+        final myAdapter adapter = new myAdapter(this, InputListe);
+        zeddel.setAdapter(adapter);
+        zeddel.setTextFilterEnabled(true);
+/*
+        //////////////
+        listView = (ListView) findViewById(R.id.listview);
+        textView = (TextView) findViewById(R.id.HeaderTextView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, testdata);
 
         listView.setAdapter(adapter);
-
-/*        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l){
-            Object listItem = listView.getItemAtPosition(i);
-        }});*/
+        //////////////
+*/
 
 
         //get back to home with home-button:
-
         Button backHome = (Button) findViewById(R.id.backHomeFromJournal);
         backHome.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,9 +110,96 @@ public class food_journal extends AppCompatActivity {
                 startActivity(myIntent);
             }
         });
-
-
-
-
     }
+
+
+    //interface ITEM:
+    public interface Item {
+        public boolean isSection();
+        public String getTitle();
+    }
+
+    public class HeaderItem implements Item{
+        private final String title;
+
+        public HeaderItem(String title) {
+            this.title = title;
+        }
+
+        @Override
+        public boolean isSection() {
+            return true;
+        }
+
+        @Override
+        public String getTitle() {
+            return title;
+        }
+    }
+
+    public class ItemItem implements Item{
+        public final String title;
+
+        public ItemItem(String title) {
+            this.title = title;
+        }
+
+        @Override
+        public boolean isSection() {
+            return false;
+        }
+
+        @Override
+        public String getTitle() {
+            return title;
+        }
+    }
+
+
+
+    //adapter:
+    public class myAdapter extends BaseAdapter {
+        private Context context;
+        private ArrayList<Item> item;
+        private ArrayList<Item> originalItem;
+
+        public myAdapter(){
+            super();
+        }
+        public myAdapter(Context context, ArrayList<Item> item){
+            this.context=context;
+            this.item=item;
+        }
+        @Override
+        public int getCount() {
+            return item.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return item.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            if (item.get(position).isSection()){
+                convertView = inflater.inflate(R.layout.list_header, parent, false);
+                TextView HeaderTextView = (TextView) convertView.findViewById(R.id.HeaderTextView);
+                HeaderTextView.setText(( item.get(position).getTitle()));
+            }
+            else{
+                convertView = inflater.inflate(R.layout.list_item,parent,false);
+                TextView ItemListView = (TextView) convertView.findViewById(R.id.ItemTextView);
+                ItemListView.setText(( item.get(position).getTitle()));
+            }
+            return convertView;
+        }
+    }
+
 }
