82a9466d2b7f33e3a56dca3ba6e38940d8463e9a
Yannik Schmidt <yannik.schmidt@thermoscan.de>
2020-06-07 21:57:03 +0200

    update db building process

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
 ## Building the Database
-Goto the [USDA Website](https://fdc.nal.usda.gov/download-datasets.html), download the **FNDDS** dataset and the **Suporting data for Downloads** and unpack it. Switch to the directory containing the CSV-Files of **BOTH** archives, then create the database with:
-
-    cut -d, -f1,2,3,4 < food_nutrient.csv > food_nutrient_cut.csv
-    split -l 56485 -d food_nutrient_cut.csv food_nutrient_cut.csv
-    sqlite3 -init $PROJECT_ROOT/sqlite3.init $PROJECT_ROOT/app/src/main/res/raw/food.db
+Goto the [USDA Website](https://fdc.nal.usda.gov/download-datasets.html), download the **FNDDS** dataset and the **Suporting data for Downloads** and unpack it into the *"helper\_scripts"*-directory. Then execute the ``make_db.py``-script. (sqlite3 + python3 is required for this).
 
 Alternatively, download the [prebuilt database](https://media.atlantishq.de/food.db).
---
+++ helper_scripts/make_db.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+import os
+
+SRC_FILE = "food_nutrient.csv"
+TMP_FILE = "food_nutrient_cut.csv"
+DB_INIT_FILE = "sqlite3.init"
+DB_INIT_FILE_NEW = "sqlite3-full.init"
+
+# only keep "id","fdc_id","nutrient_id","amount"
+os.system("rm food_nutrient_*")
+os.system("cut -d, -f1,2,3,4 < {} > {}".format(SRC_FILE, TMP_FILE))
+
+# split into equal parts
+MAX_LENGHT = 50000
+count = 0
+PARTIAL_DB_FILE_NAME = "food_nutrient_{0:02d}.csv"
+SCHEMA = '''"id","fdc_id","nutrient_id","amount"\n'''
+
+with open(TMP_FILE) as f:
+    for line in f:
+        curFileName = PARTIAL_DB_FILE_NAME.format(int(count / MAX_LENGHT))
+        if os.path.isfile(curFileName):
+            with open(curFileName, "a") as fout:
+                fout.write(line)
+        else:
+            with open(curFileName, "w") as fout:
+                fout.write(SCHEMA)
+                fout.write(line)
+
+        count += 1
+
+with open(DB_INIT_FILE) as f:
+    with open(DB_INIT_FILE_NEW, "w") as fout:
+        fout.write(f.read())
+        for x in range(0, int(count / MAX_LENGHT) + 1):
+            fout.write(".import food_nutrient_{0:02d}.csv food_nutrient_{0:02d}\n".format(x))
+
+DB_TARGET_PATH = "food.db"
+os.system("rm {}".format(DB_TARGET_PATH))
+os.system("sqlite3 -init {} {} <<EOF".format(DB_INIT_FILE_NEW, DB_TARGET_PATH))
+
+DB_TARGET_PATH = "../app/src/main/res/raw/food.db"
+os.system("rm {}".format(DB_TARGET_PATH))
+os.system("sqlite3 -init {} {} <<EOF".format(DB_INIT_FILE_NEW, DB_TARGET_PATH))
