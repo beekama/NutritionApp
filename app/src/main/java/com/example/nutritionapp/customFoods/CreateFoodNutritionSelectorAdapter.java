3ef891ec4b9acf052e848d2374baa2cd2e4a9b9b
Yannik Schmidt <yannik.schmidt@thermoscan.de>
2020-09-21 05:06:22 +0200

    implement prototype food creation

---
+++ app/src/main/java/com/example/nutritionapp/customFoods/CreateFoodNutritionSelectorAdapter.java
@@ -0,0 +1,97 @@
+package com.example.nutritionapp.customFoods;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.nutritionapp.NutritionOverview.NutritionOverview;
+import com.example.nutritionapp.R;
+import com.example.nutritionapp.foodJournal.OverviewFoodsLists.GroupFoodItem;
+import com.example.nutritionapp.foodJournal.OverviewFoodsLists.GroupListAdapter;
+import com.example.nutritionapp.other.Conversions;
+import com.example.nutritionapp.other.NutritionAnalysis;
+import com.example.nutritionapp.other.NutritionPercentageTupel;
+
+import java.util.ArrayList;
+
+public class FoodOverviewAdapter extends BaseAdapter {
+    private Context context;
+    private ArrayList<FoodOverviewListItem> items;
+
+    public FoodOverviewAdapter(Context context, ArrayList<FoodOverviewListItem> items){
+        this.context = context;
+        this.items   = items;
+    }
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        /* item at postition */
+        FoodOverviewListItem itemAtCurPos = this.items.get(position);
+
+        /* inflate layout */
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        convertView = inflater.inflate(R.layout.journal_foods_dayheader, parent, false);
+
+        /* get relevant sub-views */
+        TextView dateText = convertView.findViewById(R.id.dateText);
+        TextView energyText = convertView.findViewById(R.id.energyBar);
+        TextView nutritionText = convertView.findViewById(R.id.nutritionBar);
+        ListView subFoodList = convertView.findViewById(R.id.list_grouped_foods);
+
+        /* set the correct date */
+        dateText.setText(items.get(position).date);
+
+        dateText.setOnClickListener(view -> {
+            Log.wtf("WTF", "on click listener");
+            Intent target = new Intent(view.getContext(), NutritionOverview.class);
+            target.putExtra("startDate", items.get(position).date);
+            context.startActivity(target);
+        });
+
+        NutritionAnalysis analysis = new NutritionAnalysis(itemAtCurPos.foods);
+
+
+        /* calculate and set nutrition */
+        ArrayList<NutritionPercentageTupel> percentages = analysis.getNutritionPercentageSortedFilterZero();
+        String testText;
+        if(!percentages.isEmpty()) {
+            testText = String.format("%s : Only %d%%", percentages.get(0).nutritionElement, (int) (percentages.get(0).percentage * 100));
+        }else{
+            testText = "";
+        }
+        nutritionText.setText(testText);
+        energyText.setText(Conversions.jouleToKCal(analysis.getTotalEnergy()) + " KCAL");
+
+        /* display the foods in the nested sub-list */
+        ArrayList<GroupFoodItem> listItemsInThisSection = new ArrayList<>();
+        for(int group : itemAtCurPos.foodGroups.keySet()){
+            listItemsInThisSection.add(new GroupFoodItem(itemAtCurPos.foodGroups.get(group), group));
+        }
+        Log.wtf("FOOD", "--------");
+        ListAdapter subListViewAdapter = new GroupListAdapter(context, listItemsInThisSection);
+        subFoodList.setAdapter(subListViewAdapter);
+        return convertView;
+    }
+}
