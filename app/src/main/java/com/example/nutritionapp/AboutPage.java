dc40d3047bd3581444f2609db5f81942a42c7206
Yannik Schmidt <yannik.schmidt@thermoscan.de>
2021-06-26 13:19:05 +0200

    add toolbar to about page

--- app/src/main/java/com/example/nutritionapp/AboutPage.java
+++ app/src/main/java/com/example/nutritionapp/AboutPage.java
@@ -1,6 +1,7 @@
 package com.example.nutritionapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.text.util.Linkify;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -21,6 +23,15 @@ public class AboutPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_page);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        TextView toolbarTitle = findViewById(R.id.toolbar_title);
+        ImageButton toolbarBack = findViewById(R.id.toolbar_back);
+        toolbar.setTitle("");
+        toolbarTitle.setText(R.string.about);
+        setSupportActionBar(toolbar);
+        toolbarBack.setOnClickListener((v -> finish()));
+        toolbarBack.setImageResource(R.drawable.ic_arrow_back_black_24dp);
+
         LinearLayout layout = findViewById(R.id.aboutScrollViewLinearLayout);
 
         String[] sectionTitles = {
@@ -31,11 +42,11 @@ public class AboutPage extends AppCompatActivity {
                 "Disclaimer"
         };
         String[] sectionTexts  = {
-                "Kathrin Maurer M\n" +
+                "Kathrin Maurer\n" +
                     "Yannik Schmidt",
                 "This Software is licensed GPLv3, it contains libraries with compatible licenses listed below. "
                         + "The source code can be found here: \n\nhttps://github.com/beekama/NutritionApp/",
-                "MPAndroidChart by PhilJay licensed Apache 2.0" +
+                "MPAndroidChart by PhilJay licensed Apache 2.0\n" +
                         "Some other Project licensed GPLv2",
                 "The Data used in this project is provided by the United States Food and Drug Administration (USDA), it is in the public domain.",
                 "The information and help provided by this App is not medical advise. We do not have any affiliation with any industry entity or interest group, " +
